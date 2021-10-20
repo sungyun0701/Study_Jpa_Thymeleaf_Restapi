7477db7fd400070c5d8e8d4688b434471fae25b1
sunyun0701 <sungyun0701@gmail.com>
2021-10-20 16:17:19 +0900

    thymeleaf fragment add

--- src/main/java/jpastudy/jpashop/web/HomeController.java
+++ src/main/java/jpastudy/jpashop/web/HomeController.java
@@ -2,14 +2,20 @@ package jpastudy.jpashop.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
 public class HomeController {
-    @RequestMapping("/")
+    @RequestMapping("/frag")
     public String home() {
         log.info("home controller");
         return "home";
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
     }
 }
