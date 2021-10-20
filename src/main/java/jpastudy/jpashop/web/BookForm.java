15a3349f84de9f07fe04f10d51a669c44f7a3046
sunyun0701 <sungyun0701@gmail.com>
2021-10-20 17:44:45 +0900

    상품 등록,수정,리스트 최종

--- src/main/java/jpastudy/jpashop/web/BookForm.java
+++ src/main/java/jpastudy/jpashop/web/BookForm.java
@@ -3,12 +3,17 @@ package jpastudy.jpashop.web;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 public class BookForm {
     private Long id;
+    @NotBlank(message = "상품 이름은 필수입니다.")
     private String name;
     private int price;
+    @Min(value = 1, message = "상품 재고수량은 1개 이상이어야 합니다.")
     private int stockQuantity;
     private String author;
     private String isbn;
--- src/main/java/jpastudy/jpashop/web/ItemController.java
+++ src/main/java/jpastudy/jpashop/web/ItemController.java
@@ -6,11 +6,13 @@ import jpastudy.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -20,12 +22,16 @@ public class ItemController {
 
     @GetMapping(value = "/items/new")
     public String createForm(Model model) {
-        model.addAttribute("form", new BookForm());
+        model.addAttribute("bookForm", new BookForm());
+        //클래스로 넘겨줄때는 앞에는 소문자로 하는 네임을 적어주어야 한다.
         return "items/createItemForm";
     }
 
     @PostMapping(value = "/items/new")
-    public String create(BookForm form) {
+    public String create(@Valid BookForm form, BindingResult result) {
+        if (result.hasErrors()) {
+            return "items/createItemForm";
+        }
         Book book = new Book();
         book.setName(form.getName());
         book.setPrice(form.getPrice());
